94dc93e87485a3ad4f62fc9d96cf8e96b552f9f0
IPECTER <ipectert@gmail.com>
2022-03-21 16:03:13 +0900

    Prepare Code

--- src/main/java/com/github/ipecter/rtu/filemanageapi/RTUFileManageAPI.java
+++ src/main/java/com/github/ipecter/rtu/filemanageapi/RTUFileManageAPI.java
@@ -2,10 +2,7 @@ package com.github.ipecter.rtu.filemanageapi;
 
 import org.bukkit.plugin.Plugin;
 
-import javax.xml.crypto.Data;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Set;
 
 public class RTUFileManageAPI {
@@ -19,63 +16,7 @@ public class RTUFileManageAPI {
     public static RTUFileManageAPI getInstance() {
         return LazyHolder.INSTANCE;
     }
-
-    /**
-     * config.yml *
-     */
-    private static final int CONFIG_FILE_VERSION = 4;
-    private static final boolean CONFIG_RESET_FILE = true;
-    private static final List<String> CONFIG_IGNORED_NODES = new ArrayList<String>() {
-        {
-            add("config-version");
-        }
-    };
-    private static final List<String> CONFIG_REPLACE_NODES = new ArrayList<String>() {
-        {
-            add("disabled-worlds");
-        }
-    };
-
-    /**
-     * messages.yml *
-     */
-    private static final int MESSAGES_FILE_VERSION = 1;
-    private static final boolean MESSAGES_RESET_FILE = false;
-    private static final List<String> MESSAGES_IGNORED_NODES = new ArrayList<String>() {
-        {
-            add("config-version");
-        }
-    };
-    private static final List<String> MESSAGES_REPLACE_NODES = new ArrayList<>();
-
-    /**
-     * biomes.yml *
-     */
-    private static final int BIOMES_FILE_VERSION = 0; //Unused for now
-    private static final boolean BIOMES_RESET_FILE = false;
-    private static final List<String> BIOMES_IGNORED_NODES = new ArrayList<String>() {
-        {
-            add("config-version");
-        }
-    };
-    private static final List<String> BIOMES_REPLACE_NODES = new ArrayList<>();
-
-    /**
-     * database.db *
-     */
-    private static final int DATABASE_FILE_VERSION = 0; //Unused for now
-    private static final boolean DATABASE_RESET_FILE = false;
-
     private HashMap<String, StorageFile> fileMap = new HashMap<>();
-
-    public void reloadFiles(Plugin plugin) {
-
-        INSTANCE.fileMap.clear();
-        INSTANCE.registerFile("config.yml", new YAMLFile("config.yml"));
-        INSTANCE.registerFile("messages.yml", new YAMLFile("messages.yml"));
-        INSTANCE.registerFile("biomes.yml", new YAMLFile("biomes.yml"));
-        INSTANCE.registerFile("database.db", new DBFile("database.db"));
-    }
 
     /**
      *
@@ -83,13 +24,13 @@ public class RTUFileManageAPI {
      * @param type YAML or DB
      */
     public void reloadFile(Plugin plugin, String fileName, String type) {
-        String dir = INSTANCE.fileMap.get(fileName).directory;
-        INSTANCE.fileMap.remove(fileName);
+        String dir = fileMap.get(fileName).directory;
+        fileMap.remove(fileName);
         if (type.equalsIgnoreCase("YAML")){
-            INSTANCE.registerFile(fileName, new YAMLFile(plugin, dir, fileName));
+            registerFile(fileName, new YAMLFile(plugin, dir, fileName));
         }
         else if(type.equalsIgnoreCase("DB")){
-            INSTANCE.registerFile(fileName, new YAMLFile(plugin, dir, fileName));
+            registerFile(fileName, new YAMLFile(plugin, dir, fileName));
         }
 
     }
